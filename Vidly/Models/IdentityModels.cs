9d1ca70c81b5a5d3684675dbed0b4b84d2090c40
Erik Heath <erikheath8@gmail.com>
2021-05-22 23:29:24 -0400

    added phone number to account fields

--- Vidly/Models/IdentityModels.cs
+++ Vidly/Models/IdentityModels.cs
@@ -14,6 +14,10 @@ namespace Vidly.Models
         [StringLength(64)]
         public string DrivingLicense { get; set; }
 
+        [Required]
+        [StringLength(32)]
+        public string Phone { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
